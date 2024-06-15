4b482573a01ba55b0f06a7c44a8f09daf86d881d
RohitParmar-17 <rohitghost5050@gmail.com>
2024-06-15 21:31:17 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -6,11 +6,11 @@ import TextForm from './components/TextForm';
 import Alert from './components/Alert';
 
 import React from "react";
-// import {
-//   BrowserRouter as Router,
-//   Route,
-//   Routes
-// } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes
+} from "react-router-dom";
 
 function App() {
   const [mode,setMode] = useState('light')
@@ -43,19 +43,19 @@ function App() {
 
   return ( 
     <>
-    {/* <Router> */}
+    <Router>
       <Navbar title="TextUtils" mode={mode} toggleMode = {toggleMode}/>
       <Alert alert={alert} />
       <div className="container my-3">
-        <TextForm showAlert={showAlert} heading="Enter the text to analyze below" mode={mode}/>
-        {/* <Routes>
-          <Route exact path="/about" element={<About />}>
+        {/* <TextForm showAlert={showAlert} heading="Enter the text to analyze below" mode={mode}/> */}
+        <Routes>
+          <Route exact path="/about" element={<About mode={mode}/>}>
           </Route>
-          <Route exact path="/" element={<TextForm showAlert={showAlert} heading="Enter the text to analyze below" mode={mode}/>}>
+          <Route exact path="/" element={<TextForm showAlert={showAlert} heading="Try TextUtils - Word Counter, Character Counter, Remove extra spaces" mode={mode}/>}>
           </Route>
-        </Routes> */}
+        </Routes>
       </div>
-      {/* </Router> */}
+      </Router>
     </>
   );
 }
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -30,6 +30,7 @@ export default function TextForm(props) {
         var text = document.getElementById("myBox");
         text.select();
         navigator.clipboard.writeText(text.value);
+        document.getSelection().removeAllRanges();
         props.showAlert("Copied to Clipboard","success")
     }
 
@@ -41,22 +42,22 @@ export default function TextForm(props) {
     return (
         <>
             <div className='container' style={{color:props.mode==='light'?'black':'white'}}>
-                <h1>{props.heading}</h1>
+                <h1 className='mb-3'>{props.heading}</h1>
                 <div className="mb-3">
-                <textarea className="form-control" style={{backgroundColor:props.mode==='light'?'white':'grey' , color:props.mode==='light'?'black':'white'}} value={text} onChange={handleOnChange} id="myBox" rows="10"></textarea>
+                <textarea className="form-control" style={{backgroundColor:props.mode==='light'?'white':'#13466e' , color:props.mode==='light'?'black':'white'}} value={text} onChange={handleOnChange} id="myBox" rows="10"></textarea>
                 </div>
-                <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to Uppercase</button>
-                <button className="btn btn-primary mx-2" onClick={handleLoClick}>Convert to Lowercase</button>
-                <button className="btn btn-primary mx-2" onClick={handleClear}>Clear text</button>
-                <button className="btn btn-primary mx-2" onClick={handleCopy}>Copy text</button>
-                <button className="btn btn-primary mx-2" onClick={handleExtraSpace}>Remove Extra Space</button>
+                <button disabled={text.length===0} className="btn btn-primary mx-2 my-1" onClick={handleUpClick}>Convert to Uppercase</button>
+                <button disabled={text.length===0} className="btn btn-primary mx-2 my-1" onClick={handleLoClick}>Convert to Lowercase</button>
+                <button disabled={text.length===0} className="btn btn-primary mx-2 my-1" onClick={handleClear}>Clear text</button>
+                <button disabled={text.length===0} className="btn btn-primary mx-2 my-1" onClick={handleCopy}>Copy text</button>
+                <button disabled={text.length===0} className="btn btn-primary mx-2 my-1" onClick={handleExtraSpace}>Remove Extra Space</button>
             </div>
             <div className="container my-2" style={{color:props.mode==='light'?'black':'white'}}>
                 <h1>Your Text Summary</h1>
-                <p>{text.split(" ").length} words and {text.length} characters </p>
-                <p>{0.008*text.split(" ").length}Minutes read</p>
+                <p>{text.split(" ").filter((element)=>{return element.length!==0}).length} words and {text.length} characters </p>
+                <p>{0.008*text.split(" ").filter((element)=>{return element.length!==0}).length}Minutes read</p>
                 <h2>Preview</h2>
-                <p>{text.length>0?text:"Enter something in the textbox above to preview it here"}</p>
+                <p>{text.length>0?text:"Nothing to preview"}</p>
             </div>
         </>
   )
