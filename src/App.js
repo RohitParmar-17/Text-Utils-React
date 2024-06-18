4c513263d418ba597018720a36963075432a344e
RohitParmar-17 <159684528+RohitParmar-17@users.noreply.github.com>
2024-06-18 17:48:10 +0530

    latest commit

--- src/App.js
+++ src/App.js
@@ -58,10 +58,10 @@ function App() {
       <div className="container my-3">
         {/* <TextForm showAlert={showAlert} heading="Try TextUtils - Word Counter, Character Counter, Remove extra spaces" mode={mode}/> */}
         <Routes>
+          <Route exact path="/home" element={<TextForm showAlert={showAlert} heading="Try TextUtils - Word Counter, Character Counter, Remove extra spaces" mode={mode}/>}>
+          </Route> 
           <Route exact path="about" element={<About mode={mode}/>}>
           </Route>
-          <Route exact path="/" element={<TextForm showAlert={showAlert} heading="Try TextUtils - Word Counter, Character Counter, Remove extra spaces" mode={mode}/>}>
-          </Route> 
         </Routes>
       </div>
     </>
